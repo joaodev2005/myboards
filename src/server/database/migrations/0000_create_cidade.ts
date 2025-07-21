3a1b45781f125cacfef25a9845eb79aa62afbd7a
João Victor <123279954+joaodev2005@users.noreply.github.com>
2025-07-21 19:35:26 -0300

    feat(database): add migration and enum for cidade table

---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,23 @@
+import type { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
+
+
+export async function up(knex: Knex) {
+  return knex.schema.createTable(ETableNames.cidade, (table) => {
+    table.bigIncrements('id').primary().index();
+    table.string('nome', 150).notNullable().index();
+
+    table.comment('Tabela que armazena as cidades');
+  })
+  .then(() => {
+    console.log('# Create Table: ' + ETableNames.cidade);
+  });
+}
+
+
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(ETableNames.cidade)
+  .then(() => {
+    console.log('# Drop Table: ' + ETableNames.cidade);
+  });
+}
